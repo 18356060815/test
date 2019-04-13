a2740dcdd28d9a84f8bb6e3302401a26a265e377
huangxuyu <hxy1845510149@sina.com>
2019-04-13 15:38:43 +0800

    2019-04-13 03

--- src/main/java/com/jnk/test/crawl/DownLoadvideo_task.java
+++ src/main/java/com/jnk/test/crawl/DownLoadvideo_task.java
@@ -1,10 +1,17 @@
 package com.jnk.test.crawl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.jnk.test.Service.DBUtil;
+import com.jnk.test.util.DateUtil;
 import com.jnk.test.util.HttpClientUtilPro;
+import com.jnk.test.util.JsoupUtilPor;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringRunner;
